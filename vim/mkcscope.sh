d9498914871d93f2cea7fa535944d0ffd6dd342b
Travis CI <travis@travis-ci.org>
2018-07-05 00:42:20 +0900

    add *.hpp

--- vim/mkcscope.sh
+++ vim/mkcscope.sh
@@ -3,5 +3,5 @@
  # use this file after copy to /usr/bin
  #
  rm -rf cscope.files cscope.files
- find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print > cscope.files
+ find . \( -name '*.c' -o -name '*.cpp' -o -name '*.hpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print > cscope.files
  cscope -i cscope.files
