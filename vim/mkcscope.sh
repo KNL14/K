ea96886d832282ad65d599aa17f77586ec820b16
ThomasKr17 <thomaskr2017@gmail.com>
2018-09-17 20:31:46 +0900

    /usr/bin -> /usr/local/bin으로 변경함. 별거아님

--- vim/mkcscope.sh
+++ vim/mkcscope.sh
@@ -1,6 +1,6 @@
  #!/bin/sh
 
- # use this file after copy to /usr/bin
+ # use this file after copy to /usr/local/bin
  #
  rm -rf cscope.files cscope.files
  find . \( -name '*.c' -o -name '*.cpp' -o -name '*.hpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print > cscope.files
